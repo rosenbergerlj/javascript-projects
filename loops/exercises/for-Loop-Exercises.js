e19c83097a86b29a695ba6897380669e0e02ef58
Joe Rosenberger <joerosenberger@joes-air.attlocal.net>
2024-06-26 10:07:21 -0500

    for-Loop-Exercises 6.26.2024

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -1,10 +1,27 @@
+const { lstat } = require("fs")
+
 /*Exercise #1: Construct for loops that accomplish the following tasks:
     a. Print the numbers 0 - 20, one number per line.
     b. Print only the ODD values from 3 - 29, one number per line.
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
-
-
+//a
+for (let i = 0; i < 21; i++) {
+  console.log(i);
+}
+//b
+for (let i = 3; i < 30; i = i + 2) {
+  console.log(i);
+}
+//c
+for (let i = 12; i > -15; i = i - 2) {
+  console.log(i);
+}
+//d
+for (let i = 50; i > 19; i = i - 1) {
+if (i % 3 === 0)
+  console.log(i);
+}
 
 
 /*Exercise #2: 
@@ -14,7 +31,20 @@ Initialize two variables to hold the string “LaunchCode” and the array [1, 5
 Construct ``for`` loops to accomplish the following tasks:
   a. Print each element of the array to a new line.
   b. Print each character of the string - in reverse order - to a new line. */ 
+  let courseName = "LanchCode";
+  let loopArray = [1, 5, "LC101", "blue", 42];
+  let reversed = "";
+   
+  for (let i = 0; i < loopArray.length; i++) {
+      console.log(loopArray[i]);
+    }
 
+  for (let i = 0; i < courseName.length; i++) {
+      reversed = courseName[i] + reversed;
+  }
+  for (let i = 0; i < reversed.length; i++) {
+      console.log(reversed[i]);
+  };
 
 
 
@@ -22,3 +52,16 @@ Construct ``for`` loops to accomplish the following tasks:
 /*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
   a. One array contains the even numbers, and the other holds the odds.
   b. Print the arrays to confirm the results. */
+let sortArray = [2, 3, 13, 18, -5, -38, -10, 11, 0, 104]
+let evens = [];
+let odds = [];
+
+for (let i = 0; i < sortArray.length; i++) {
+  if (sortArray[i] % 2 ===0) {
+    evens.push(sortArray[i]);
+    } else (odds.push(sortArray[i]));
+  }
+  console.log(evens);
+  console.log(odds);
+
+  
