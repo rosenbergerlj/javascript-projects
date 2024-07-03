ddf1a7f53493615d7d8cc43cb82dacf80985f2df
Joe Rosenberger <joerosenberger@joes-air.attlocal.net>
2024-07-02 19:12:14 -0500

    Studio Exercise 7.1.2024

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -4,5 +4,18 @@ let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Sort each array in ascending order.
+nums1.sort(function(a, b){return a-b});
+nums2.sort(function(a, b){return a-b});
+nums3.sort(function(a, b){return a-b});
+console.log(nums1);
+console.log(nums2);
+console.log(nums3);
+
 
 //Sort each array in descending order.
+nums1.sort(function(a, b){return b-a});
+nums2.sort(function(a, b){return b-a});
+nums3.sort(function(a, b){return b-a});
+console.log(nums1);
+console.log(nums2);
+console.log(nums3);
