0e12bec7a332483b1383fee6ba4ddacad5f5e4ab
Joe Rosenberger <rosenbergerlj@gmail.com>
2024-08-04 13:42:03 -0500

    dom-and-event exercise 8.4.2024

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,24 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener("click", function() {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+    
+    missionAbort.addEventListener("mouseover", function(event){
+        event.target.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", function(event) {
+        event.target.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", function() {
+        let response = window.confirm("Are you sure you want to abort the mission?");
+        if (response === true) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home.";    
+        }
+    });
     
 }
 
